de4ca9ef83a0814922cb94b11d637a5cc9f72ed5
Dane Naebers <danenaebers@gmail.com>
2021-12-26 23:23:21 +0100

    add more auth providers

--- src/components/forms/SignInForm.tsx
+++ src/components/forms/SignInForm.tsx
@@ -1,53 +1,52 @@
+import "firebaseui/dist/firebaseui.css";
+
 import * as firebaseui from "firebaseui";
 import firebase from "../../utils/firebaseClient";
-import {Card, Col, Row} from "antd";
+import {Col, Row} from "antd";
 import {GradientHeader} from "../shared/GradientHeader";
 import React from "react";
-import "firebaseui/dist/firebaseui.css";
 import FirebaseAuth from "../FirebaseAuth";
 
 const config: firebaseui.auth.Config = {
     signInFlow: "popup",
     signInOptions: [
+        "microsoft.com",
         firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
     ],
-}
-
-export const SignInForm = () => {
-    return (
-        <>
-            <Row justify="center">
-                <GradientHeader
-                    className="app-title"
-                    text="Retro Next"
-                    gradient={"90deg, rgb(112, 88, 255) 35%, rgb(1, 183, 240) 100%"}
-                    fontSize="60px"
-                    fontWeight={600}
-                />
-            </Row>
-            <Row justify="center">
-                <Col>
-                    <Card>
-                        <FirebaseAuth uiConfig={config} firebaseAuth={firebase.auth()}/>
-                    </Card>
-                </Col>
-            </Row>
-
-            <style jsx>
-                {`
-                  :global(.app-title) {
-                    margin: 80px 0 20vh !important;
-                  }
-
-                  .sign-in-form {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                  }
-                `}
-            </style>
-        </>
-
-    )
 };
+
+export const SignInForm: React.FC = (): JSX.Element => (
+    <>
+        <Row justify="center">
+            <GradientHeader
+                className="app-title"
+                text="Retro Next"
+                gradient={"90deg, rgb(112, 88, 255) 35%, rgb(1, 183, 240) 100%"}
+                fontSize="60px"
+                fontWeight={600}
+            />
+        </Row>
+        <Row justify="center">
+            <Col>
+                <FirebaseAuth uiConfig={config} firebaseAuth={firebase.auth()}/>
+            </Col>
+        </Row>
+
+        <style jsx>
+            {`
+              :global(.app-title) {
+                margin: 80px 0 20vh !important;
+              }
+
+              .sign-in-form {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              }
+            `}
+        </style>
+    </>
+
+);
